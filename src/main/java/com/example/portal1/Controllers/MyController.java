6d5062ebede92a9b4607d1fdcf1dcc96401e035d
siddhartha2001 <siddharthaveluvolu@gmail.com>
2022-04-21 20:23:13 +0530

    Updated JenaWork

--- src/main/java/com/example/portal1/Controllers/MyController.java
+++ src/main/java/com/example/portal1/Controllers/MyController.java
@@ -28,4 +28,40 @@ public class MyController {
         System.out.println(queryString);
         return resultSet;
 	}
+	
+	@CrossOrigin
+	@RequestMapping("/seeker/appliedjobs")
+	public List<JSONObject> getAppliedJobDetails(@RequestParam String seeker_name){
+		String queryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+				+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "
+				+ "PREFIX portal: <http://www.iiitb.ac.in/IMT2018071/DM/portal1#> "
+				+ "SELECT ?y ?z WHERE {"
+				+ " portal:"
+				+ seeker_name
+				+ " portal:AppliesFor ?z . "
+				+ "?y portal:Offers ?z"
+				+ "}";
+		
+		List<JSONObject> resultSet = InitJena.describeClass(queryString);
+        System.out.println(queryString);
+        return resultSet;	
+	}
+	
+	@CrossOrigin
+	@RequestMapping("/search/company")
+	public List<JSONObject> getJobsCompany(@RequestParam String company_name){
+		String queryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+				+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "
+				+ "PREFIX portal: <http://www.iiitb.ac.in/IMT2018071/DM/portal1#> "
+				+ "SELECT ?z WHERE {"
+				+ " portal:"
+				+ company_name
+				+ " portal:Offers ?z"
+				+ "}";
+		
+		List<JSONObject> resultSet = InitJena.getItems(queryString);
+        System.out.println(queryString);
+        return resultSet;
+	}
+	
 }
--- src/main/java/com/example/portal1/JenaWork/InitJena.java
+++ src/main/java/com/example/portal1/JenaWork/InitJena.java
@@ -32,9 +32,7 @@ public class InitJena {
 
                 //Execute the query and obtain results
                 qe = QueryExecutionFactory.create(query, ontoModel);
-                System.out.println(qe);
                 ResultSet results = qe.execSelect();
-                System.out.println(results);
 
                 // Output query results
                 //ResultSetFormatter.out(System.out, results, query);
@@ -61,11 +59,51 @@ public class InitJena {
             QuerySolution solution = resultSet.nextSolution();
             System.out.println(solution);
 
-            obj.put(solution.get("y").toString().split("#")[1],solution.get("z").toString().split("#")[1]);
+            String y = solution.get("y").toString();
+            if(y.contains("^")) {
+            	y = y.substring(0, y.indexOf('^'));
+            }
+            else if(y.contains("#")) {
+            	y = y.split("#")[1];
+            }
+            
+            String z = solution.get("z").toString();
+            if (z.contains("^")) {
+            	z = z.substring(0, z.indexOf('^'));
+            }
+            else if(z.contains("#")) {
+            	z = z.split("#")[1];
+            }
+                        
+            obj.put(y,z);
             list.add(obj);
         }
         return list;
     }   
+    
+    public static List<JSONObject> getItems(String queryString){
+    	System.out.println(queryString);
+    	ResultSet resultSet = execQuery(queryString);
+        List<JSONObject> list = new ArrayList<>();
+        int x=0;
+        while (resultSet.hasNext()) {
+        	x++;
+            JSONObject obj = new JSONObject();
+            QuerySolution solution = resultSet.nextSolution();          
+            
+            String z = solution.get("z").toString();
+            if (z.contains("^")) {
+            	z = z.substring(0, z.indexOf('^'));
+            }
+            else if(z.contains("#")) {
+            	z = z.split("#")[1];
+            }
+                        
+            obj.put(Integer.toString(x),z);
+            list.add(obj);
+        }
+        return list;
+    }
     
     
     
